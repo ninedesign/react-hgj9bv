f9815573baf185965f8682b1db8158cafe5983bd
Stefano Bossi <nineusa@gmail.com>
2019-10-15 20:58:15 -0600

    Updating
    Updating

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import "./index.scss";
+
+// function Card({ title, inset, children, ...others }) {
+//     return (
+//         <div {...others} className="card">
+//             <div className="card-body">
+//                 <h5 className="card-title"><span>{inset}</span>{title}</h5>
+//                 <p className="card-text">
+//                     {children}
+//                 </p>
+//             </div>
+//         </div>
+//     );
+// }
 
 function Card({ title, children, ...others }) {
     return (
@@ -13,7 +27,6 @@ function Card({ title, children, ...others }) {
         </div>
     );
 }
-
 Card.propTypes = {
     title: PropTypes.string.isRequired,
     // eslint-disable-next-line react/forbid-prop-types
