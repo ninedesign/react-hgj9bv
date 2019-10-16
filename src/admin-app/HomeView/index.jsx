19329b3133ae40c0e5ae300789a4ec89e28883a1
Stefano Bossi <nineusa@gmail.com>
2019-10-15 20:54:54 -0600

    Updating
    updating

--- src/admin-app/HomeView/index.jsx
+++ src/admin-app/HomeView/index.jsx
@@ -1,24 +1,46 @@
-import React from 'react';
+import React from "react";
+import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
-import './index.scss';
-import PageTitle from '../PageTitle';
-import Card from '../../components/Card';
+import "./index.scss";
+import PageTitle from "../PageTitle";
+import Card from "../../components/Card";
 
 function HomeView() {
-    return (
-        <div className="home-view">
-            <PageTitle title="Home" />
-            <Card title="Card title">
-                Hello from Card 1
-            </Card>
+  // for body anchor links (cards)
+  let anchorRef = React.createRef()
+
+  return (
+    <div className="home-view">
+      <PageTitle title="Home - Configurations" />
+      <div className="container">
+        <div className="row justify-content-around">
+          <div className="col col-sm-5 col-md-6">
+            <Card title="Report Configurations">Short description.<Link to="/reports" innerRef={anchorRef} className="stretched-link" /></Card>
+          </div>
+          <div className="col col-sm-5 col-md-6">
+            <Card title="Report Type Configurations"><Link to="/reporttypes" innerRef={anchorRef} className="stretched-link" />Short description.</Card>
+          </div>
         </div>
-    );
+        <div className="row justify-content-around">
+          <div className="col col-sm-5 col-md-6">
+            <Card title="Asset Type Configurations"><Link to="/assettypes" innerRef={anchorRef} className="stretched-link" />Short description.</Card>
+          </div>
+          <div className="col col-sm-5 col-md-6">
+            <Card title="Export Report Configurations"><Link to="/exportreport" innerRef={anchorRef} className="stretched-link" />Short description.</Card>
+          </div>
+        </div>
+        <div className="row justify-content-around">
+          <div className="col col-sm-5 col-md-6">
+            <Card title="Miscellaneous Configurations"><Link to="/miscellaneous" innerRef={anchorRef} className="stretched-link" />Short description.</Card>
+          </div>
+          </div>
+      </div>
+    </div>
+  );
 }
 
-HomeView.propTypes = {
-};
+HomeView.propTypes = {};
 
-HomeView.defaultProps = {
-};
+HomeView.defaultProps = {};
 
 export default HomeView;
